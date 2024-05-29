f324d7ee83345c117bf9abdcdc388dc291b8cb57
oovaa <omarvx211@gmail.com>
2024-05-29 06:51:32 +0200

    add try catch at 29-06

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,15 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default async function handleProfileSignup() {
-  let a = uploadPhoto();
-  let b = createUser();
-  let ans = await Promise.all([a, b]);
-  let body = ans[0].body;
-  let fname = ans[1].firstName;
-  let lname = ans[1].lastName;
-  console.log([body, fname, lname].join(' '));
+  try {
+    let a = uploadPhoto();
+    let b = createUser();
+    let ans = await Promise.all([a, b]);
+    let body = ans[0].body;
+    let fname = ans[1].firstName;
+    let lname = ans[1].lastName;
+    console.log([body, fname, lname].join(' '));
+  } catch (e) {
+    console.log('Signup system offline');
+  }
 }
