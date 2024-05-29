d8e091da435efd306568a21fdc5e12d17a7dbd2b
oovaa <omarvx211@gmail.com>
2024-05-29 11:02:28 +0200

    task A 10 at 29-11

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,17 @@
+import { createUser, uploadPhoto } from './utils.js';
+
+export default async function asyncUploadUser() {
+  let phres = uploadPhoto();
+  let ures = createUser();
+  let ans;
+  try {
+    ans = await Promise.all([phres, ures]);
+  } catch (e) {
+    ans = [null, null];
+  }
+
+  return {
+    photo: ans[0],
+    user: ans[1]
+  };
+}
