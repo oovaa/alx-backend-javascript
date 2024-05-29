039ce77d19cf43dc095f073e6569a5abd76cbed5
oovaa <omarvx211@gmail.com>
2024-05-29 10:31:53 +0200

    task 9 done at 29-10

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction) {
+  let queue = [];
+  let val;
+  try {
+    val = mathFunction();
+  } catch (error) {
+    val = 'Error: ' + error.message;
+  } finally {
+    queue.push(val, 'Guardrail was processed');
+  }
+
+  return queue;
+}
