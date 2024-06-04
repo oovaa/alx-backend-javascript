23c4006d98cb0463364c8152de9f53cedf9c4821
oovaa <omarvx211@gmail.com>
2024-06-04 15:13:19 +0200

    chore: Add error handling for API endpoint load in queryAPI function 2

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -5,10 +5,9 @@ function queryAPI(endpoint) {
     weakMap.set(endpoint, 0);
   }
   let last = weakMap.get(endpoint);
-  if (last >= 5) {
-    throw new Error('Endpoint load is high');
-  }
+
   weakMap.set(endpoint, last + 1);
+  if (last >= 5) throw new Error('Endpoint load is high');
 }
 
 export { queryAPI, weakMap };
