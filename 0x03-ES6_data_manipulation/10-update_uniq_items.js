b16b6ac3bf99e1033fc53df1b66561f5cf680c7e
oovaa <omarvx211@gmail.com>
2024-06-04 14:53:47 +0200

    add error handle task 10 at 04-14

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -3,6 +3,9 @@
  * @param {Map} map
  */
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
   map.forEach((v, k) => {
     if (v == 1) map.set(k, 100);
   });
