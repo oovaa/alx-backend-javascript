73a4a1643d50e31181e95577fc7f7bcaf23a5e95
oovaa <omarvx211@gmail.com>
2024-07-23 13:16:27 +0200

    started 0x05. NodeJS Basics at 23-13

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,24 @@
+interface Student {
+    lastName: string
+    firstName: string
+    age: number
+    location: string
+}
+
+
+const student: Student = {
+    lastName: "Doe",
+    firstName: "John",
+    age: 20,
+    location: "New York"
+};
+
+const student2: Student = {
+    lastName: "omar",
+    firstName: "abdo",
+    age: 20,
+    location: "sudan K"
+};
+
+
+let studentsList: Student[] = []
